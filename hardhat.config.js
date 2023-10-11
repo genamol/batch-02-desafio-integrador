42bbf7c50f4c05a9819cd79af8e1f7a673b8e870
Genaro Molina <genamolina01@gmail.com>
2023-10-11 15:06:04 -0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
 require("dotenv").config();
 
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
